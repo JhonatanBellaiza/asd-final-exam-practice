b2c5dda679a49527b87d60b78213d931a6b32bd0
Jhonatan <jhonatan_bellaiza@hotmail.com>
2024-11-24 19:15:50 -0600

    first commit

---
+++ src/main/java/edu/miu/asdpracticefinalexam/AsdPracticeFinalExamApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.asdpracticefinalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdPracticeFinalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdPracticeFinalExamApplication.class, args);
+    }
+
+}
